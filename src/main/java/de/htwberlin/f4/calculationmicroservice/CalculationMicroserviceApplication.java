8332d8b16ef61e4536f2cb78ab40f8084cb1a9e2
GlebTanaka <gleb5655@gmail.com>
2021-12-04 15:56:22 +0100

    first commit

---
+++ src/main/java/de/htwberlin/f4/calculationmicroservice/CalculationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.f4.calculationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/f4/calculationmicroservice/CalculationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.f4.calculationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
