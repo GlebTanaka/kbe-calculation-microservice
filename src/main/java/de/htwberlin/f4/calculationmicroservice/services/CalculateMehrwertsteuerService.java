e6488f7c6a494d7289e2068c6f1e63c44db65ad4
User5867 <lindi.f01@gmail.com>
2022-01-19 18:08:02 +0100

    cleanup

--- src/main/java/de/htwberlin/f4/calculationmicroservice/services/CalculateMehrwertsteuerService.java
+++ src/main/java/de/htwberlin/f4/calculationmicroservice/services/CalculateMehrwertsteuerService.java
@@ -2,16 +2,10 @@ package de.htwberlin.f4.calculationmicroservice.services;
 
 import org.springframework.stereotype.Service;
 
-/**
- * Service für das berrechnen der Mehrwertsteuer
- */
 @Service
 public class CalculateMehrwertsteuerService {
     /**
-     * Berrechnet die die Mehrwertsteuer die noch auf den Preis gerrechnet werden muss
-     * @param preis vor Steuern
-     * @return Mehrwertsteuer als double wenn der Preis größer als 0 ist
-     * @throws IllegalArgumentException wenn der preis negativ oder 0 ist
+     * @throws IllegalArgumentException if price is Negative or Zero
      */
     public double calculateMehrwertSteuer(double preis) throws IllegalArgumentException{
         if(preis<=0){
