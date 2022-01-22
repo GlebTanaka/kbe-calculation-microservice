4a78c51f728c77a30c28602698753fdc4b735d56
GlebTanaka <gleb5655@gmail.com>
2022-01-22 14:56:06 +0100

    Add SpringFox devendecies to enable SwaggerDocs
    Hat to downgrade version of spring to, otherwise there were conflicts
    with SpringFox

---
+++ src/main/java/de/htwberlin/f4/calculationmicroservice/config/SpringFoxConfig.java
@@ -0,0 +1,9 @@
+package de.htwberlin.f4.calculationmicroservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+}
